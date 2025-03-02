3693dae1a89c40d7e8b5d3e6ed4ba602b6533ecb
kevoc <kevoc@github>
2025-03-02 15:24:35 +0100

    Turning off caching so the script runs every time

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ LOCAL_ISO_PATH=${1:?}
 LOCAL_PRESEED_PATH=${2:?}
 OUTPUT_DIR=${3:?}
 
-docker build \
+docker build --no-cache \
 	--build-arg LOCAL_ISO_PATH="$LOCAL_ISO_PATH" \
 	--build-arg LOCAL_PRESEED_PATH="$LOCAL_PRESEED_PATH" \
 	--output type=local,dest="$OUTPUT_DIR" \
